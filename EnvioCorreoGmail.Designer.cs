1aada9eed6f9f4a3e546fb542eb9a5e5ffbf16b6
tarbusa <tarbusa@gmail.com>
2020-10-31 17:50:56 -0500

    Envio de correo usando SMTP GMAIL

---
+++ EnvioCorreoGmail.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace EnviarMail_net_Core
+{
+    partial class EnvioCorreoGmail
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.correo = new System.Windows.Forms.TextBox();
+            this.enviar = new System.Windows.Forms.Button();
+            this.asunto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cuerpo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // correo
+            // 
+            this.correo.Location = new System.Drawing.Point(60, 11);
+            this.correo.Name = "correo";
+            this.correo.Size = new System.Drawing.Size(409, 20);
+            this.correo.TabIndex = 0;
+            // 
+            // enviar
+            // 
+            this.enviar.Location = new System.Drawing.Point(475, 8);
+            this.enviar.Name = "enviar";
+            this.enviar.Size = new System.Drawing.Size(111, 62);
+            this.enviar.TabIndex = 1;
+            this.enviar.Text = "enviar";
+            this.enviar.UseVisualStyleBackColor = true;
+            this.enviar.Click += new System.EventHandler(this.enviar_Click);
+            // 
+            // asunto
+            // 
+            this.asunto.Location = new System.Drawing.Point(60, 50);
+            this.asunto.Name = "asunto";
+            this.asunto.Size = new System.Drawing.Size(409, 20);
+            this.asunto.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Correo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Asunto: ";
+            // 
+            // cuerpo
+            // 
+            this.cuerpo.Location = new System.Drawing.Point(15, 102);
+            this.cuerpo.Multiline = true;
+            this.cuerpo.Name = "cuerpo";
+            this.cuerpo.Size = new System.Drawing.Size(571, 152);
+            this.cuerpo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Descripcion:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 266);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cuerpo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.asunto);
+            this.Controls.Add(this.enviar);
+            this.Controls.Add(this.correo);
+            this.Name = "Form1";
+            this.Text = "Envio de Correo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox correo;
+        private System.Windows.Forms.Button enviar;
+        private System.Windows.Forms.TextBox asunto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cuerpo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EnvioCorreoGmail.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnviarMail_net_Core
+{
+    public partial class EnvioCorreoGmail : Form
+    {
+        public EnvioCorreoGmail()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void clear()
+        {
+            correo.Clear();
+            asunto.Clear();
+            cuerpo.Clear();
+        }
+        private void enviarMail(string addressfrom, String subject, String body)
+        {
+            try
+            {
+                //string ruta = "D:/Carga.csv"; //Ruta del codumento que se adjuntara en el correo (OPCIONAL)
+                //string addressfrom = "correoDestino@gmail.com"; //Correo destino
+                string addressto = "miEmpresa@gmail.com"; //Servidor de correo, en este caso usare un correo secundario que creé para efectos de practica
+                string password = "miContraseña123"; //Contraseña del correo
+                int port = 587; //Puerto por defecto de SMPT de GMAIL 
+                //string subject = "El asunto del correo";
+                //string body = "Cuerpo del correo o todo el contenido del correo";
+                string server = "smtp.gmail.com"; //Servidor de correo del GMAIL
+
+                SmtpClient SmtpServer = new SmtpClient(server);
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(addressfrom);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.To.Add(addressfrom);
+
+                /* //para adjuntar documentos dentro del mail
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment(ruta);
+                mail.Attachments.Add(attachment);
+                */
+    
+                SmtpServer.Port = port;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(addressto, password);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Mensaje enviado!");
+                clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al enviar correo: "+ex);
+            }
+        }
+        private void enviar_Click(object sender, EventArgs e)
+        {
+            String mail = correo.Text;
+            String subject = asunto.Text;
+            String body = cuerpo.Text;
+            enviarMail(mail, subject, body);
+
+        }
+    }
+}
